fcd7bb220eb3003c4752b250f2b50e69025cee1f
waj <wangaj@glodon.com>
2018-11-10 13:47:20 +0800

    将主题设置成无ActionBar的主题 把toolbar当标题来用

--- app/src/main/java/com/waj/testcoordinatelayout/MainActivity.kt
+++ app/src/main/java/com/waj/testcoordinatelayout/MainActivity.kt
@@ -2,11 +2,22 @@ package com.waj.testcoordinatelayout
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Window
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        actionBar?.let { it.hide() }
+
+        toolbar.title = "title"
+        toolbar.setLogo(R.mipmap.ic_launcher)
+        toolbar.subtitle = "child title"
+        setSupportActionBar(toolbar)
     }
 }
